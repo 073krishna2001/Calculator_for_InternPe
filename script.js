3c1597b1bf651d65476e397e4b0cc891e5ca6968
Krishna Pratap Singh <96362071+073krishna2001@users.noreply.github.com>
2023-07-12 13:12:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const toggle = document.querySelector('.toggle')
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if(html.classList.contains('dark')){
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark mode'
+    }
+    else{
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light mode'
+    }
+})
+
+
+let outputScreen = document.getElementById("output-screen");
+
+function display(num) {
+    outputScreen.value += num;
+}
+function calculate() {
+    try {
+        outputScreen.value = eval(outputScreen.value);
+    }
+    catch (err) {
+        alert("Invalid")
+    }
+}
+function Clear() {
+    outputScreen.value = "";
+}
+function del() {
+    outputScreen.value = outputScreen.value.slice(0, -1);
+}
